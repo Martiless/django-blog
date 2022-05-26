4040d6b491cbe6d71b406ad395886997eea3d322
Martina Martin <marti.law@gmail.com>
2022-05-26 10:45:18 +0000

    Edit blog.urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
-    path('', include('blog.urls'), name='blog_urls'),
+    path("", views.PostList.as_view(), name="home"),
 ]
